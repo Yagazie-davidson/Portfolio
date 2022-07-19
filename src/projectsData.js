98a6423fe7f03758a9dbaa27d869d16b0b765a08
Letam Davidson <davidsonletam@gmail.com>
2022-07-19 07:08:25 +0100

    test

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,44 +1,45 @@
-import helloDevs from './assets/hello-devs.png'
-import nft from './assets/nft1.png'
-export default[
-   {
-       id: 1,
-       title: "Hello Devs",
-       img: helloDevs,
-       description: "This is a random tech quote generator. It was built using HTML, CSS and JavaScript. In this project I practiced the use of APIs. When the App first loads, it pulls data from an API then renders in on the UI.",
-       direction: true,
-       github: "https://github.com/Yagazie-davidson/Hello-devs.git",
-       website: "https://hellodevs.netlify.app/",
-       html: true
-   },
-{
-       id: 2,
-       title: "NFT Landing Page",
-       img: nft,
-       description: "An NFT landing page built with HTML and CSS and Tailwind CSS. It was translated to code frorm a design in the Figma community",
-       direction: true,
-       github: 'https://github.com/Yagazie-davidson/NFT-Landing-page',
-       website: 'https://nftlp.netlify.app/'
-   },
-   // {
-   //     id: 2,
-   //     title: "Clean touch",
-   //     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
-   //     description: "A landing page built with HTML and CSS only. lorem iyhehiwhqw dishiwhiw weiwiywuihwuh wehweihuihuiwe.",
-   //     direction: true
-   // },
-   // {
-   //     id: 2,
-   //     title: "Mad",
-   //     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
-   //     description: "lorem iyhehiwhqw dishiwhiw. weiwiywuihwuh .wehweihuihuiwe.",
-   //     direction: false
-   // },
-   // {
-   //     id: 3,
-   //     title: "Mad",
-   //     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
-   //     description: "lorem iyhehiwhqw dishiwhiw. weiwiywuihwuh .wehweihuihuiwe.",
-   //     direction: true
-   // }
-]
+import helloDevs from "./assets/hello-devs.png";
+import nft from "./assets/nft1.png";
+export default [
+	{
+		id: 1,
+		title: "Hello Devs",
+		img: helloDevs,
+		description:
+			"This is a random tech quote generator. It was built using HTML, CSS and JavaScript. In this project I practiced the use of APIs. When the App first loads, it pulls data from an API then renders in on the UI.",
+		direction: true,
+		github: "https://github.com/Yagazie-davidson/Hello-devs.git",
+		website: "https://hellodevs.netlify.app/",
+		html: true,
+	},
+	// {
+	//        id: 2,
+	//        title: "NFT Landing Page",
+	//        img: nft,
+	//        description: "An NFT landing page built with HTML and CSS and Tailwind CSS. It was translated to code frorm a design in the Figma community",
+	//        direction: true,
+	//        github: 'https://github.com/Yagazie-davidson/NFT-Landing-page',
+	//        website: 'https://nftlp.netlify.app/'
+	//    },
+	// {
+	//     id: 2,
+	//     title: "Clean touch",
+	//     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
+	//     description: "A landing page built with HTML and CSS only. lorem iyhehiwhqw dishiwhiw weiwiywuihwuh wehweihuihuiwe.",
+	//     direction: true
+	// },
+	// {
+	//     id: 2,
+	//     title: "Mad",
+	//     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
+	//     description: "lorem iyhehiwhqw dishiwhiw. weiwiywuihwuh .wehweihuihuiwe.",
+	//     direction: false
+	// },
+	// {
+	//     id: 3,
+	//     title: "Mad",
+	//     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
+	//     description: "lorem iyhehiwhqw dishiwhiw. weiwiywuihwuh .wehweihuihuiwe.",
+	//     direction: true
+	// }
+];
