04609fcbf24e6b2927313fe2c1de80e1a364c072
Letam Ugwuadu <77066722+Yagazie-davidson@users.noreply.github.com>
2023-04-24 05:19:54 -0700

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -83,6 +83,7 @@ function AboutSection(props) {
 		align-items: center;
 		justify-content: flex-start;
 		column-gap: 3rem;
+		width: 100%;
 		padding-top: 5rem;
 		@media screen and (max-width: 414px) {
 			display: grid;
