66592e9ef8bb083f5c7e02ee124f94825fe40597
letam <davidsonletam@gmail.com>
2023-07-09 16:44:06 +0100

    new project

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,68 +1,78 @@
 import helloDevs from "./assets/hello-devs.png";
 import nft from "./assets/nft1.png";
 export default [
-{
-		id: 4,
-		title: "SERVICE BUDS",
-		img: "https://i.ibb.co/gWBb0qH/Screenshot-147.png", 
-		description:
-			"A Full-Stack web application for University of Lagos student to post their services and for others students to get easy access to this information. This is really helpful to student, most espeicaily the ones new to a particular hostel. Services like Electrician, Carpenter, Barber and other essentials to every hostel will be posted. Students are able to filter the services to get services in a particular hostel. The closer the service provider, the better and faster the process will be. Service providers are required to post some certain informations that will be stored in a database",
-		direction: true,
-		github: "https://github.com/Yagazie-davidson/service-buds-client",
-		website: "https://servicebuds.vercel.app/",
-		html: true,
-	},
-	{
-		id: 1,
-		title: "HC - BARBERS",
-		img: "https://i.ibb.co/wwH2fLK/HC.png",
-		description:
-			"A Barbing saloon website built with NEXT JS",
-		direction: true,
-		github: "https://github.com/Yagazie-davidson/Hair_cut_barbers",
-		website: "https://hc-barbers.vercel.app/",
-		html: true,
-	},
-	{
-		id: 2,
-		title: "HELLO DEVS",
-		img: helloDevs,
-		description:
-			"This is a random tech quote generator. It was built using HTML, CSS and JavaScript. In this project I practiced the use of APIs. When the App first loads, it pulls data from an API then renders in on the UI.",
-		direction: true,
-		github: "https://github.com/Yagazie-davidson/Hello-devs.git",
-		website: "https://hellodevs.netlify.app/",
-		html: true,
-	},
-	{
-		id: 3,
-		title: "NFT LANDING PAGE",
-		img: nft,
-		description:
-			"An NFT landing page built with HTML and CSS and Tailwind CSS. It was translated to code frorm a design in the Figma community",
-		direction: true,
-		github: "https://github.com/Yagazie-davidson/NFT-Landing-page",
-		website: "https://nftlp.netlify.app/",
-	},
-	// {
-	//     id: 2,
-	//     title: "Clean touch",
-	//     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
-	//     description: "A landing page built with HTML and CSS only. lorem iyhehiwhqw dishiwhiw weiwiywuihwuh wehweihuihuiwe.",
-	//     direction: true
-	// },
-	// {
-	//     id: 2,
-	//     title: "Mad",
-	//     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
-	//     description: "lorem iyhehiwhqw dishiwhiw. weiwiywuihwuh .wehweihuihuiwe.",
-	//     direction: false
-	// },
-	// {
-	//     id: 3,
-	//     title: "Mad",
-	//     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
-	//     description: "lorem iyhehiwhqw dishiwhiw. weiwiywuihwuh .wehweihuihuiwe.",
-	//     direction: true
-	// }
+  {
+    id: 4,
+    title: "SERVICE BUDS",
+    img: "https://i.ibb.co/gWBb0qH/Screenshot-147.png",
+    description:
+      "A Full-Stack web application for University of Lagos student to post their services and for others students to get easy access to this information. This is really helpful to student, most espeicaily the ones new to a particular hostel. Services like Electrician, Carpenter, Barber and other essentials to every hostel will be posted. Students are able to filter the services to get services in a particular hostel. The closer the service provider, the better and faster the process will be. Service providers are required to post some certain informations that will be stored in a database",
+    direction: true,
+    github: "https://github.com/Yagazie-davidson/service-buds-client",
+    website: "https://servicebuds.vercel.app/",
+    html: true,
+  },
+  {
+    id: 6,
+    title: "Finance Flow",
+    img: "https://i.ibb.co/xgVYj8k/ff.png",
+    description:
+      "A Landing page for an Organization. Inspired by the Figma community.",
+    direction: true,
+    github: "https://github.com/Yagazie-davidson/finanace_flow",
+    website: "https://finanaceflow.vercel.app/",
+    html: true,
+  },
+  {
+    id: 3,
+    title: "NFT LANDING PAGE (Desktop only)",
+    img: nft,
+    description:
+      "An NFT landing page built with HTML and CSS and Tailwind CSS. It was translated to code frorm a design in the Figma community, Warning: Desktop only",
+    direction: true,
+    github: "https://github.com/Yagazie-davidson/NFT-Landing-page",
+    website: "https://nftlp.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "HC - BARBERS",
+    img: "https://i.ibb.co/wwH2fLK/HC.png",
+    description: "A Barbing saloon website built with NEXT JS",
+    direction: true,
+    github: "https://github.com/Yagazie-davidson/Hair_cut_barbers",
+    website: "https://hc-barbers.vercel.app/",
+    html: true,
+  },
+  //   {
+  //     id: 2,
+  //     title: "HELLO DEVS",
+  //     img: helloDevs,
+  //     description:
+  //       "This is a random tech quote generator. It was built using HTML, CSS and JavaScript. In this project I practiced the use of APIs. When the App first loads, it pulls data from an API then renders in on the UI.",
+  //     direction: true,
+  //     github: "https://github.com/Yagazie-davidson/Hello-devs.git",
+  //     website: "https://hellodevs.netlify.app/",
+  //     html: true,
+  //   },
+  // {
+  //     id: 2,
+  //     title: "Clean touch",
+  //     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
+  //     description: "A landing page built with HTML and CSS only. lorem iyhehiwhqw dishiwhiw weiwiywuihwuh wehweihuihuiwe.",
+  //     direction: true
+  // },
+  // {
+  //     id: 2,
+  //     title: "Mad",
+  //     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
+  //     description: "lorem iyhehiwhqw dishiwhiw. weiwiywuihwuh .wehweihuihuiwe.",
+  //     direction: false
+  // },
+  // {
+  //     id: 3,
+  //     title: "Mad",
+  //     img: "https://janinemarsh.dev/images/RokFitness%20Portfolio%20preview.png",
+  //     description: "lorem iyhehiwhqw dishiwhiw. weiwiywuihwuh .wehweihuihuiwe.",
+  //     direction: true
+  // }
 ];
